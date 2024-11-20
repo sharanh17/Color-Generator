e4445ca2a45c04187196a2e4967012a220a69473
sharan <sharanhidaduggi17@gmail.com>
2024-11-20 12:07:18 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import RandomColor from "./components/generatecolors";
+
+function App() {
+  return (
+    <>
+      <RandomColor />
+    </>
+  );
+}
+
+export default App;
